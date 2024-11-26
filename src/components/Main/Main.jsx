948ecf29f21d720e365a7a0270bdbf959bc61f10
JeanPoolDev <jeanpoolrojasavila456@gmail.com>
2024-11-26 17:22:24 -0500

    add-api

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,24 @@
+import { useContext } from 'react';
 import { assets } from '../../assets/assets';
 import { cardContent } from '../../helpers/cardContent';
 import './Main.css';
+import { Context } from '../../context/Context';
 
 export const Main = () => {
+
+  const {
+    onSent,
+    input,
+    setInput,
+    recentPrompt,
+    setRecentPrompt,
+    prevPrompt,
+    setPrevPrompt,
+    showResult,
+    loading,
+    resultData } = useContext(Context);
+
+
   return (
     <div className='main'>
 
@@ -12,30 +28,55 @@ export const Main = () => {
       </header>
 
       <section className="main-container">
-        <div className="greet">
-          <p><span>Hola. ¿Cómo estás?</span></p>
-          <p>Que aventura nos espera...</p>
-        </div>
-        <div className='cards'>
 
-          {
-            cardContent.map(card => (
-              <div key={card.title} className='card'>
-                <p>{card.message}</p>
-                <img src={card.img} alt={card.title} />
-              </div>
-            ))
-          }
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p><span>Hola. ¿Cómo estás?</span></p>
+              <p>Que aventura nos espera...</p>
+            </div>
+            <div className='cards'>
 
-        </div>
+              {
+                cardContent.map(card => (
+                  <div key={card.title} className='card'>
+                    <p>{card.message}</p>
+                    <img src={card.img} alt={card.title} />
+                  </div>
+                ))
+              }
+
+            </div>
+          </>
+        ) :
+          <div className='result'>
+
+            <div className="result-title">
+              <img src={assets.user_icon} alt="user-icon" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="gemini-icon" />
+              {loading
+                ? <div className='loader'> <hr /><hr /><hr /> </div>
+                : <p dangerouslySetInnerHTML={{ __html: resultData }} ></p>
+              }
+            </div>
+
+          </div>
+        }
 
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder='Ingresa un prompt..' />
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder='Ingresa un prompt..' />
             <div>
               <img src={assets.gallery_icon} alt="gallery-icon" />
               <img src={assets.mic_icon} alt="mic_icon" />
-              <img src={assets.send_icon} alt="send_icon" />
+              <img onClick={() => onSent()} src={assets.send_icon} alt="send_icon" />
             </div>
           </div>
           <p className='bottom-info'>
